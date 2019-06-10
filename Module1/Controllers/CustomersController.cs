085a88c37a11687460673b809bcf6bdc292d3c38
KBANKNET\nussaba.k <nussaba.k@kbtg.tech>
2019-06-10 14:54:37 +0700

    add CustomerController , Customer Model

---
+++ Module1/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Module1.Models;
+
+namespace Module1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        static List<Customer> _customers = new List<Customer>()
+        {
+            new Customer() { Id=0 , Name= "Andrew",  Email ="andrew@gmail.com" , Phone = "555"},
+            new Customer() { Id=1 , Name= "Tom",  Email ="tom@gmail.com" , Phone = "111"}
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_customers);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Customer customer)
+        {
+            _customers.Add(customer);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}
